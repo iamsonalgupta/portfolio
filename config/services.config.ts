c3b0afc05d5e4b5a3bbf0ebe9d5181841b057523
Sonal <sonalgupta0330@gmail.com>
2024-12-29 20:20:22 +0530

    update

--- config/services.config.ts
+++ config/services.config.ts
@@ -7,7 +7,7 @@ export const smtp = {
   host: 'smtp.sendgrid.net',
   port: 465,
   username: 'apikey',
-  password: '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',
+  password: '',
 
   fromEmail: 'admin@iamsonalgupta.com',
   fromName: 'Team SYSOTEL.AI',
