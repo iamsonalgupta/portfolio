a9eb049c85181e83ba1d1c3db2155c4571be8311
Sonal <sonalgupta0330@gmail.com>
2024-12-30 00:02:37 +0530

    redirect www to base

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,21 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.iamsonalgupta.com', // Replace with your domain
+          },
+        ],
+        destination: 'https://iamsonalgupta.com/:path*', // Replace with your root domain
+        permanent: true,
+      },
+    ];
+  },
   output: "export",
   trailingSlash: true,
 };
